0fe33628ac1252431ea8c76fd3680bf11e6efa1d
CristianLopez21 <cristianlopez200977@gmail.com>
2025-11-30 12:18:39 -0500

    Debug dafault as True

--- Modulo/Modulo/settings.py
+++ Modulo/Modulo/settings.py
@@ -15,13 +15,10 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-load_dotenv(BASE_DIR / 'variables.env')
+load_dotenv(BASE_DIR / 'DB_variables.env')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8bs%eqb#v&wbou-=8c8_rir#uci#gp!fn-&f#o1e5!z^%2dewb'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -76,7 +73,7 @@ WSGI_APPLICATION = 'Modulo.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 SECRET_KEY = os.environ.get("SECRET_KEY")
-DEBUG = os.environ.get("DEBUG", "False") == "True"
+DEBUG = os.environ.get("DEBUG", "True") == "True"
 
 DATABASES = {
     "default": {
