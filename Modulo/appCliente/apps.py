5fcc3ca0c4403218ed81c4fece28b20e7f1ea326
CristianLopez21 <cristianlopez200977@gmail.com>
2025-12-05 16:57:54 -0500

    Reorganizacion del proyecto Django

--- Modulo/appCliente/apps.py
+++ Modulo/appCliente/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class Modulo2Config(AppConfig):
+class AppclienteConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'Modulo2'
+    name = 'appCliente'
