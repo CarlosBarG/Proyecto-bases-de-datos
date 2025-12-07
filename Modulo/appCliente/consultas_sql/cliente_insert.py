adcb5a04b2383ad918e1ef71b2843e022d7ef210
CristianLopez21 <cristianlopez200977@gmail.com>
2025-12-07 13:10:16 -0500

    Se agregaron estilos con css para que el formulario se vea como el solicitado

--- Modulo/appCliente/consultas_sql/cliente_insert.py
+++ Modulo/appCliente/consultas_sql/cliente_insert.py
@@ -1,5 +1,4 @@
 
-
 sql_insert_cliente = """
     INSERT INTO CLIENTE (CODCLIENTE, IDTIPODOC, NOMCLIENTE, APELLCLIENTE, NDOCUMENTO)
     VALUES (:codcliente, :idtipodoc, :nomcliente, :apellcliente, :ndocumento)
